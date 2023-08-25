ad0cf59027458cef4b66e4224d6063a905686d79
Matthew Cravinhos <mcrav14@matthews-mbp-2.lan>
2023-08-24 21:10:18 -0500

    Added view and fixed table names

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- creating tables with no foreign keys first otherwise there will be errors
 -- make sure to use DESCRIBE to show tables work in SQLFiddle for screenshots
 
-CREATE TABLE COFFEE_SHOP (
+CREATE TABLE Coffee_Shop (
     shop_id INT,
     shop_name VARCHAR(50),
     city VARCHAR(50),
@@ -11,7 +11,7 @@ CREATE TABLE COFFEE_SHOP (
     PRIMARY KEY (shop_id)
 );
 
-CREATE TABLE SUPPLIER (
+CREATE TABLE Supplier (
     supplier_id INT,
     company_name VARCHAR(50),
     country VARCHAR(30),
@@ -20,7 +20,7 @@ CREATE TABLE SUPPLIER (
     PRIMARY KEY (supplier_id)
 );
 
-CREATE TABLE EMPLOYEE (
+CREATE TABLE Employee (
     employee_id INT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
@@ -28,18 +28,18 @@ CREATE TABLE EMPLOYEE (
     job_title VARCHAR(30),
     shop_id INT,
     PRIMARY KEY (employee_id),
-    FOREIGN KEY (shop_id) REFERENCES COFFEE_SHOP (shop_id)
+    FOREIGN KEY (shop_id) REFERENCES Coffee_Shop (shop_id)
 );
 
-CREATE TABLE COFFEE (
+CREATE TABLE Coffee (
     coffee_id INT,
     shop_id INT,
     supplier_id INT,
     coffee_name VARCHAR(30),
     price_per_pound NUMERIC(5,2)
     PRIMARY KEY (coffee_id),
-    FOREIGN KEY (shop_id) REFERENCES COFFEE_SHOP (shop_id),
-    FOREIGN KEY (supplier_id) REFERENCES SUPPLIER (supplier_id)
+    FOREIGN KEY (shop_id) REFERENCES Coffee_Shop (shop_id),
+    FOREIGN KEY (supplier_id) REFERENCES Supplier (supplier_id)
 );
 
 
@@ -48,25 +48,25 @@ CREATE TABLE COFFEE (
 
 -- using multi-line value inserts, not sure if UPDATE is needed in some form
 
-INSERT INTO COFFEE_SHOP(shop_id,shop_name,city,state)
+INSERT INTO Coffee_Shop(shop_id,shop_name,city,state)
 VALUES
     (1,"Houndstooth Coffee","Austin","TX"),
     (2,"Laughing Man","New York City","NY"),
     (3,"The Wormhole Coffee","Chicago","IL");
 
-INSERT INTO SUPPLIER(supplier_id,company_name,country,sales_contact_name,email)
+INSERT INTO Supplier(supplier_id,company_name,country,sales_contact_name,email)
 VALUES
     (4,"Big Bean LLC","Mexico","Juan","juan@bigbean.com"),
     (5,"Firewood Roasters","United States","Derek","derek@firewoodroasters.com"),
     (6,"Coffee Bean Exporters Brazil","Brazil","Marcelo","marcelo@exportcoffee.com");
 
-INSERT INTO EMPLOYEE(employee_id,first_name,last_name,hire_date,job_title,shop_id)
+INSERT INTO Employee(employee_id,first_name,last_name,hire_date,job_title,shop_id)
 VALUES
     (7,"Bruce","Wayne",2008-11-11,"Night Roaster", 17),
     (8,"Tony","Stark",2010-07-15,"Iron Barista", 18),
     (9,"Peter","Parker",2014-04-18,"Spider Manager", 19);
 
-INSERT INTO COFFEE(coffee_id,shop_id,supplier_id,coffee_name,price_per_pound)
+INSERT INTO Coffee(coffee_id,shop_id,supplier_id,coffee_name,price_per_pound)
 VALUES
     (10,20,30,"Big Bean Dark",13.49),
     (11,21,31,"Firewood Medium",11.99),
@@ -76,4 +76,17 @@ VALUES
 -- step b3
 
 
--- create view
+CREATE VIEW full_name AS
+    SELECT
+        employee_id,
+        first_name,
+        last_name,
+        hire_date,
+        job_title,
+        shop_id,
+        CONCAT(first_name,' ',last_name) AS employee_full_name
+    FROM
+        Employee;
+
+
+-- step b4
