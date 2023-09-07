1ecfe1af84c74c1474cba4d577fb1d51edcd3bf7
Matthew Cravinhos <matthewscravinhos@gmail.com>
2023-09-06 20:49:17 -0500

    Fixed foreign key issue in INSERT blocks

--- database.sql
+++ database.sql
@@ -46,29 +46,29 @@ CREATE TABLE Coffee (
 -- step B2
 
 
-INSERT INTO Coffee_Shop(shop_id,shop_name,city,state)
+INSERT INTO Coffee_Shop(shop_id, shop_name, city, state)
 VALUES
-    (1,"Houndstooth Coffee","Austin","TX"),
-    (2,"Laughing Man","New York City","NY"),
-    (3,"The Wormhole Coffee","Chicago","IL");
+    (1, 'Houndstooth Coffee', 'Austin', 'TX'),
+    (2, 'Laughing Man', 'New York City', 'NY'),
+    (3, 'The Wormhole Coffee', 'Chicago', 'IL');
 
-INSERT INTO Supplier(supplier_id,company_name,country,sales_contact_name,email)
+INSERT INTO Supplier(supplier_id, company_name, country, sales_contact_name, email)
 VALUES
-    (4,"Big Bean LLC","Mexico","Juan","juan@bigbean.com"),
-    (5,"Firewood Roasters","United States","Derek","derek@firewoodroasters.com"),
-    (6,"Coffee Bean Exporters Brazil","Brazil","Marcelo","marcelo@exportcoffee.com");
+    (4, 'Big Bean LLC', 'Mexico', 'Juan', 'juan@bigbean.com'),
+    (5, 'Firewood Roasters', 'United States', 'Derek', 'derek@firewoodroasters.com'),
+    (6, 'Coffee Bean Exporters Brazil', 'Brazil', 'Marcelo', 'marcelo@exportcoffee.com');
 
-INSERT INTO Employee(employee_id,first_name,last_name,hire_date,job_title,shop_id)
+INSERT INTO Employee(employee_id, first_name, last_name, hire_date, job_title, shop_id)
 VALUES
-    (7,"Bruce","Wayne","2008-11-11","Night Roaster", 17),
-    (8,"Tony","Stark","2010-07-15","Iron Barista", 18),
-    (9,"Peter","Parker","2014-04-18","Spider Manager", 19);
+    (7, 'Bruce', 'Wayne', '2008-11-11', 'Night Roaster', 1),
+    (8, 'Tony', 'Stark', '2010-07-15', 'Iron Barista', 2),
+    (9, 'Peter', 'Parker', '2014-04-18', 'Spider Manager', 3);
 
-INSERT INTO Coffee(coffee_id,shop_id,supplier_id,coffee_name,price_per_pound)
+INSERT INTO Coffee(coffee_id, shop_id, supplier_id, coffee_name, price_per_pound)
 VALUES
-    (10,20,30,"Big Bean Dark",13.49),
-    (11,21,31,"Firewood Medium",11.99),
-    (12,20,30,"Brazil Light",8.99);
+    (10, 1, 4, 'Big Bean Dark', 13.49),
+    (11, 2, 5, 'Firewood Medium', 11.99),
+    (12, 3, 6, 'Brazil Light', 8.99);
 
 
 -- step b3
